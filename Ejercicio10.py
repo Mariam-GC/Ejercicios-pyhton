f329eb0428480a890efc89ab68f7f0f9b1367fc2
Mariam-GC <121530920+Mariam-GC@users.noreply.github.com>
2024-07-18 21:35:22 -0600

    Add exercises 10 and 11

---
+++ Ejercicio10.py
@@ -0,0 +1,7 @@
+#Structured data in python
+#Structured data in Python is data that allows a variable to store it as more than one value, data, or data type. Such as a set of numbers, a string of characters, letters, list, sequence, etc.
+#simply create a list, declare a variable but store the elements or items in it within square brackets
+objects = ["house", "car", "ball"]
+numbers = [111, 112, 234] 
+#show one part of the list
+print(objects[1:2])
---
+++ Ejercicio11.py
@@ -0,0 +1,11 @@
+#Use funtion "type ()" for know name of variable
+
+caja1 = "Hola mundo"
+caja2 = 23
+caja3 = True
+
+print(caja1,type(caja1))
+print(caja2,type(caja2))
+print(caja3,type(caja3))
+
+
