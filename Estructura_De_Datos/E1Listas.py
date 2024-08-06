4da4d88ca92fbfd21158a7732ab8f7fd6ecfab2c
Mariam-GC <121530920+Mariam-GC@users.noreply.github.com>
2024-08-05 18:02:15 -0600

    Add file estructura de datos

---
+++ Estructura_De_Datos/E1Listas.py
@@ -0,0 +1,10 @@
+#Crea una lista llamada frutas que contenga las siguientes frutas: manzana, banana, cereza.
+#Imprime la lista frutas.
+#Agrega "naranja" a la lista frutas.
+#Imprime la lista frutas nuevamente.
+
+fruit = ["apple", "banana", "cherry"]
+print (fruit)
+#.apend agrega un nuevo elemento a la lista
+fruit.append("orange")
+print(fruit)
